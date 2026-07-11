dbe248b54f15db88df65507579113c0c6c4cd061
Rodrigomoll <victor.mollocondo@tecsup.edu.pe>
2026-07-11 15:11:36 -0600

    add maps content with local images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://Rodrigomoll.github.io',
-  base: 'earthscience',
+  base: '/earthscience/',
   integrations: [tailwind()],
 });
